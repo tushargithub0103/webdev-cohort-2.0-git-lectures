57fcfdeea8142ed37a8e65384896bee534eaff02
tushargithub0103 <codertushar01@gmail.com>
2026-02-12 15:29:33 +0530

    fix: correct console.log statement and add additional greeting message

--- hello.js
+++ hello.js
@@ -5,4 +5,6 @@ console.log(fname + " " + lname);
 
 console.log(`Hello ${fname} ${lname}`);
 
-log("Hello " + fname + " " + lname);
+console.log("Hello " + fname + " " + lname);
+
+console.log(fname + " " + lname + " is a good boy" + " and he is a good student");
